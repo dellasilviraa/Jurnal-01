19a9c7f6b4b36cc10373c53f88d196cba35a53f3
dellasilviraa <silviradella03@gmail.com>
2023-09-27 09:35:28 +0700

    Jurnal01

---
+++ Jurnal01.java
@@ -0,0 +1,7 @@
+public class Jurnal01 {
+    public static void main(String[] args) {
+        System.out.println("Nama : Della Silvira");
+        System.out.println("Nim : 607062300095");
+        System.out.println("Kelas : D3IF-47-04");
+    }
+}
